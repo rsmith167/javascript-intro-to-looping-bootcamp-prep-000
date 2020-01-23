8cbea993905a972cf92a747e64f477c86409d578
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 19:05:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,8 @@
-var array[]
-
 function forLoop(array){
   
   for( var i = 0; i < 25; i++){
     
-    [...array, `I am ${i} strange loop.`]
+  array.push(`I am ${i} strange loop.`)
   }
   return array
 }
