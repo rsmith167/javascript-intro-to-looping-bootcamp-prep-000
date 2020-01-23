7cc0710533a7072151b66d3897bc20574f98f321
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 19:18:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,4 +23,14 @@ function whileLoop(number){
 }
 function doWhileLoop(number){
   
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+do{
+  console.log("I run once regardless.")
+} while (incrementVariable() < number)
+  
 }
